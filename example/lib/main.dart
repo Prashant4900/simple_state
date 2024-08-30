6ab9ca548bf0b4f0628741994ada8721feac8a44
Prashant Nigam <62440911+Prashant4900@users.noreply.github.com>
2024-08-31 00:15:45 +0530

    remove combine builder

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,21 +17,22 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: const MySimplePage(),
     );
   }
 }
 
-class MySimplePage extends StatelessWidget {
-  MySimplePage({super.key});
+final counterState = SimpleState(10.0);
 
-  final counter = SimpleState<int>(0);
+class MySimplePage extends StatelessWidget {
+  const MySimplePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text("'Flutter Demo Home Page'"),
+        title: const Text("Flutter Demo Home Page"),
       ),
       body: Center(
         child: Column(
@@ -41,7 +42,52 @@ class MySimplePage extends StatelessWidget {
               'You have pushed the button this many times:',
             ),
             SimpleBuilder(
-                state: counter,
+                state: counterState,
+                builder: (context, count) {
+                  return Text(
+                    '$count',
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  );
+                }),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const MySimplePageSecond(),
+                  ),
+                );
+              },
+              child: const Text("Move"),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counterState.value += 1,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class MySimplePageSecond extends StatelessWidget {
+  const MySimplePageSecond({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              '2 Page',
+            ),
+            SimpleBuilder(
+                state: counterState,
                 builder: (context, count) {
                   return Text(
                     '$count',
@@ -52,67 +98,7 @@ class MySimplePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value += 1,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  MyHomePage({super.key});
-
-  final counter = SimpleState<int>(10);
-  final isLoading = SimpleState<bool>(false);
-  final username = SimpleState<String>("Guest");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: CombinedStateBuilder(
-          stateStreams: [
-            counter.stream,
-            isLoading.stream,
-            username.stream,
-          ],
-          builder: (context, values) {
-            final count = values[0] as int? ?? 0;
-            final loading = values[1] as bool? ?? false;
-            final user = values[2] as String? ?? "Guest";
-
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Count: $count',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Text(
-                  'Loading: $loading',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Text(
-                  'User: $user',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                TextButton(
-                    onPressed: () {
-                      username.value = "Prashant";
-                    },
-                    child: const Text("data"))
-              ],
-            );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => counter.value += 1,
+        onPressed: () => counterState.value += 1,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
--- lib/simple_state.dart
+++ lib/simple_state.dart
@@ -1,6 +1,5 @@
 /// A Very Good Project created by Very Good CLI.
 library;
 
-export 'src/combined_state_builder.dart';
 export 'src/simple_builder.dart';
 export 'src/simple_state.dart';
